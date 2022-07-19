4d8f5cb7730d106ff299f54e02d12c87dbe8ae68
toniess <ignat.glushkov@mail.ru>
2022-07-19 23:41:47 +0600

    server update
    added: 
    1. Server knows if user back
    2. Server can create new rooms

--- server/server.go
+++ server/server.go
@@ -1,11 +1,35 @@
 package main
 
 import (
-	// ch "./channels"
-	"fmt"
+	"math/rand"
 	"net"
 	"strings"
+	"time"
+	"strconv"
+
+	"fmt"
 )
+
+type Room struct {
+	ID      string
+	players []User
+}
+
+var RoomList map[string]Room = make(map[string]Room)
+
+//roomID is unique key that belongs to each room
+func generateRoomID() string {
+	//bind seed to timer
+	rand.Seed(time.Now().UnixNano())
+	for {
+		// generate new roomId until it unique
+		id := rand.Intn(9000) + 1000
+		_, ok := RoomList[string(id)]
+		if !ok {
+			return strconv.Itoa(id)
+		}
+	}
+}
 
 type User struct {
 	ID        string
@@ -13,88 +37,85 @@ type User struct {
 	replyChan chan string
 }
 
-var UserList map[string]User = make(map[string]User)
+var UserList map[chan string]User = make(map[chan string]User)
 
+// To identify user we need a unique key.
+// Remote addres is bad in case user changes wi-fi point or connection way.
+// So every client generate own unique key named deviceID,
+// using which we can identify client and connect it back to room
+// if for example connection lost
 func identifyUser(id string, newReplyChan chan string) {
-	user, ok := UserList[id]
-	if ok {
-		user.replyChan = newReplyChan
-		UserList[id] = user
-		userBack(id)
-	} else {
-		UserList[id] = User{ID: id, room: "", replyChan: newReplyChan}
+	for replyChan, user := range UserList{
+		// if there was user with same deviceID,
+		// it means client has reconnected and we have to
+		// change channels by deleting old and creating a new one
+		if user.ID == id {
+			UserList[newReplyChan] = user
+			delete(UserList, replyChan)
+			return
+		}
 	}
-}
 
+	// if there wasn't user with same deviceID,
+	// it is new user
+	UserList[newReplyChan] = User{ID: id, room: "", replyChan: newReplyChan}
+}
+// That function calls if client had lost connection and then,
+//it returned back
+//It's supposed to send all info back to client
 func userBack(id string) {
-	sendToClient(id, "ALL USER DATA")
+	// sendToClient(id, "ALL USER DATA")
 	// send all user info to client
 }
 
-func sendToClient(id, data string) {
-	replyChan := getReplyChan(id)
-	replyChan <- data
-}
-
-func getReplyChan(id string) chan string {
-	user := UserList[id]
-	return user.replyChan
-}
-
 func main() {
-	// создание сервера
+	// server creation
 	dstream, err := net.Listen("tcp", ":5893")
 	if err != nil {
 		return
 	}
 	defer dstream.Close()
 
-	// обработка новых подключений в цикле
+	// handle new connections in a loop
 	for {
-		// принять новое подключение
+		// accept new connection
 		con, err := dstream.Accept()
 		if err != nil {
 			return
 		}
-
-		//обработка подключения в новом виртуальном потоке
+		// procced connection above in separated virtual thread
 		go handle(con)
 	}
 }
 
 func handle(con net.Conn) {
 	defer con.Close()
-	//
-	// defer delete(UserList, "1011377391")
-	//
-
+	// create new channel to send replies
 	replyChan := make(chan string)
-
-	//функция для получения запросов от клиента
+	// get new client requests in loop in new thread
 	go func() {
-		// обработка запросов в цикле
 		for {
 			buf := make([]byte, 128)
-			rlen, err := con.Read(buf) // принять запрос
-
-			//проверка на наличие ошибок и верификация подключения
+			rlen, err := con.Read(buf) // get request
+			//error check
 			if err != nil {
 				return
 			}
-
-			// отправка данных в канал для обработки
+			// send request pack to parse it via function
 			go parseRequest(string(buf[:rlen]), replyChan)
 		}
 	}()
 
-	//отправка ответов клиенту
+	//sending replies to client in the loop
 	for {
 		reply := <-replyChan
 		con.Write([]byte(reply + "#"))
 	}
 }
-
+//Requests may come together
+//so we need to split it to single ones
 func parseRequest(request string, replyChan chan string) {
+	//requests are separated with '#'
 	requestList := strings.Split(request, "#")
 	for _, singleRequest := range requestList {
 		if singleRequest != "" {
@@ -104,17 +125,27 @@ func parseRequest(request string, replyChan chan string) {
 }
 
 func proceedRequest(request string, replyChan chan string) {
-	fmt.Println(request)
-	switch strings.Split(request, ":")[1] {
 
+	switch strings.Split(request, ":")[1] {
+	// client sends connection check requests
+	// and it is needed to be answered
 	case "CONN":
 		replyChan <- "REP:CONN:OK"
 
+	// after every connection client sends devideID key.
+	// It's supposed to identify user and recover his
+	// session if needed
 	case "DEVICEID":
 		go identifyUser(strings.Split(request, ":")[2], replyChan)
 
+	// this request sends if client is needed in new room
+	// It's supposed to create new room and
+	// move current user into that room
 	case "NEWROOM":
-		replyChan <- "REP:NEWROOM:1000"
-		// update user info
+		roomID := generateRoomID()
+		replyChan <- ("REP:NEWROOM:" + roomID)
+		user := UserList[replyChan]
+		user.room = roomID
+		RoomList[roomID] = Room{ID: roomID, players: []User{user}}
 	}
 }
